4183de6b557759325986db87b5f4ce1740bcbb40
Leonardo Toledo <d2025010842@unifei.edu.br>
2025-06-23 00:44:13 -0300

    Habilitando login atráves do email e senha

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:to_do_list/pages/auth_page.dart';
 import 'package:to_do_list/pages/loading_page.dart';
@@ -7,19 +8,30 @@ import 'package:to_do_list/services/auth/auth_service.dart';
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({super.key});
 
+  Future<void> init(BuildContext context) async {
+    await Firebase.initializeApp();
+  } 
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder(
+    return FutureBuilder(
+      future: init(context),
+      builder: (context, snapshot){
+        if(snapshot.connectionState == ConnectionState.waiting){
+            return LoadingPage();
+          }else {
+      return StreamBuilder(
         stream: AuthService().userChanges, 
         builder: (context, snapshot){
           if(snapshot.connectionState == ConnectionState.waiting){
             return LoadingPage();
           }else{
             return snapshot.hasData ? ToDoListPage() : AuthPage();
-          }
-        },
-      ),
-    );
+            }
+          },
+        );
+      }
+    } 
+   ); 
   }
 }
