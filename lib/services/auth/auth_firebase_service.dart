3c72584cdc7150b10715ef55b2dbaa34f76ffecc
Leonardo Toledo <d2025010842@unifei.edu.br>
2025-06-25 11:36:01 -0300

    Término da função de login com o firebase

--- lib/services/auth/auth_firebase_service.dart
+++ lib/services/auth/auth_firebase_service.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:to_do_list/models/log_user.dart';
@@ -10,7 +11,7 @@ class AuthFirebaseService implements AuthService {
   static final _userStream = Stream<LogUser?>.multi((controller) async {
     final authChanges = FirebaseAuth.instance.authStateChanges();
     await for (final user in authChanges) {
-      _currentUser = user == null ? null : _toChatUser(user);
+      _currentUser = user == null ? null : _toLogUser(user);
       controller.add(_currentUser);
     }
   });
@@ -43,8 +44,10 @@ class AuthFirebaseService implements AuthService {
     final imageName = '${credential.user!.uid}.jpg';
     final imageURL = await _uploadUserImage(image, imageName);
 
-    credential.user?.updateDisplayName(name);
-    credential.user?.updatePhotoURL(imageURL);
+    await credential.user?.updateDisplayName(name);
+    await credential.user?.updatePhotoURL(imageURL);
+
+    await _saveLogUser(_toLogUser(credential.user!, imageURL));
   }
 
   @override
@@ -69,12 +72,23 @@ class AuthFirebaseService implements AuthService {
     return await imageRef.getDownloadURL();
   }
 
-  static LogUser _toChatUser(User user) {
+  Future<void> _saveLogUser(LogUser user) async{
+    final store = FirebaseFirestore.instance;
+    final docRef = store.collection('users').doc(user.id);
+
+     return docRef.set({
+      'name': user.name,
+      'email': user.email,
+      'imageURL': user.imageURL
+    });
+  }
+
+  static LogUser _toLogUser(User user, [String? imageURL]) {
     return LogUser(
       id: user.uid,
       name: user.displayName ?? user.email!.split('@')[0],
       email: user.email!,
-      imageURL: user.photoURL ?? 'assets/images/avatar.png',
+      imageURL: imageURL ?? user.photoURL ?? 'assets/images/avatar.png',
     );
   }
 }
